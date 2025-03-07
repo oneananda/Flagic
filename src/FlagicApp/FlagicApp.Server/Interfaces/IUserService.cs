7f90f75a4f23d0a4b9c49111fca95df408dfdb51
oneananda <anandathirumurugan@gmail.com>
2025-03-07 21:19:22 +0530

    IUserService

---
+++ src/FlagicApp/FlagicApp.Server/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using FlagicApp.Server.Models;
+
+namespace FlagicApp.Server.Interfaces
+{
+    public interface IUserService
+    {
+        IEnumerable<User> GetAllUsers();
+        User GetUserById(int id);
+        User CreateUser(User user);
+        User UpdateUser(int id, User user);
+        bool DeleteUser(int id);
+    }
+}
