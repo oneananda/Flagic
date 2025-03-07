bc5cffc940c2a4a53fef282203fff432ac233328
oneananda <anandathirumurugan@gmail.com>
2025-03-07 21:21:04 +0530

    Adding UserController

---
+++ src/FlagicApp/FlagicApp.Server/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using FlagicApp.Server.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlagicApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        // Injecting the user service via constructor
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+    }
+}
