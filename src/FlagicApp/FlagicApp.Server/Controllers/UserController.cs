173ac44d24125dc1b6f21425159593ad58971f97
oneananda <anandathirumurugan@gmail.com>
2025-03-07 21:23:54 +0530

    Implementing IUserService

--- src/FlagicApp/FlagicApp.Server/Controllers/UserController.cs
+++ src/FlagicApp/FlagicApp.Server/Controllers/UserController.cs
@@ -23,5 +23,51 @@ namespace FlagicApp.Server.Controllers
         {
             return _users;
         }
+        public User GetUserById(int id)
+        {
+            return _users.FirstOrDefault(u => u.Id == id);
+        }
+
+        public User CreateUser(User user)
+        {
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
+
+            user.Id = _nextId++;
+            _users.Add(user);
+            return user;
+        }
+
+        public User UpdateUser(int id, User user)
+        {
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
+
+            var existingUser = _users.FirstOrDefault(u => u.Id == id);
+            if (existingUser == null)
+            {
+                // Optionally, you could throw an exception here
+                return null;
+            }
+
+            // Update properties
+            existingUser.FirstName = user.FirstName;
+            existingUser.LastName = user.LastName;
+            existingUser.Email = user.Email;
+
+            return existingUser;
+        }
+
+        public bool DeleteUser(int id)
+        {
+            var user = _users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return false;
+            }
+
+            _users.Remove(user);
+            return true;
+        }
     }
 }
