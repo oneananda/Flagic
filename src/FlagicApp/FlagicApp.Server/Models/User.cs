e7ad43d92c4be0b103ec5d73d142c573aecf445e
oneananda <anandathirumurugan@gmail.com>
2025-03-07 21:17:34 +0530

    Adding User class

---
+++ src/FlagicApp/FlagicApp.Server/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlagicApp.Server.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string? LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+    }
+}
