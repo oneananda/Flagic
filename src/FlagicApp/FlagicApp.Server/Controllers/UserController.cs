45c5667cac545edfa829d700aae27787665c6e69
oneananda <anandathirumurugan@gmail.com>
2025-03-07 21:21:55 +0530

    GetAllUsers

--- src/FlagicApp/FlagicApp.Server/Controllers/UserController.cs
+++ src/FlagicApp/FlagicApp.Server/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using FlagicApp.Server.Interfaces;
+using FlagicApp.Server.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,11 +10,18 @@ namespace FlagicApp.Server.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
+        private readonly List<User> _users = new List<User>();
+        private int _nextId = 1;
 
         // Injecting the user service via constructor
         public UserController(IUserService userService)
         {
             _userService = userService;
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return _users;
         }
     }
 }
