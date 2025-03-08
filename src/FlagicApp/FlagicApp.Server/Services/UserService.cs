6dda1ec94bc2e4194f1b42e7b91ca1747a023dad
oneananda <anandathirumurugan@gmail.com>
2025-03-08 08:47:46 +0530

    Adding UserService

---
+++ src/FlagicApp/FlagicApp.Server/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using FlagicApp.Server.Interfaces;
+using FlagicApp.Server.Models;
+
+namespace FlagicApp.Server.Services
+{
+    public class UserService : IUserService
+    {
+        public User CreateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User UpdateUser(int id, User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
